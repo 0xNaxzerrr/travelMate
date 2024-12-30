3344ad9dd6b7303deb8ae8059518836541ea02db
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 11:50:52 +0100

    Mise à jour du module principal avec le module Voyages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TravelsModule } from './travels/travels.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
-    AuthModule
+    AuthModule,
+    TravelsModule
   ],
   controllers: [],
   providers: [],
