8a89615bcaed3172ebd49aacaae60aa2e773b816
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 11:51:46 +0100

    Mise à jour du module principal avec le module Itinéraires

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TravelsModule } from './travels/travels.module';
+import { ItinerariesModule } from './itineraries/itineraries.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { TravelsModule } from './travels/travels.module';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
     AuthModule,
-    TravelsModule
+    TravelsModule,
+    ItinerariesModule
   ],
   controllers: [],
   providers: [],
