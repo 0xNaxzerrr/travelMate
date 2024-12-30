28b2e2b6fc4891f536efaf59bc18cb05bf5c94eb
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 12:31:49 +0100

    Mise à jour du module de Géolocalisation avec nouveaux services

--- backend/src/geolocation/geolocation.module.ts
+++ backend/src/geolocation/geolocation.module.ts
@@ -1,15 +1,1 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { GeolocationService } from './geolocation.service';
-import { GeolocationController } from './geolocation.controller';
-import { Geolocation, GeolocationSchema } from './schemas/geolocation.schema';
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Geolocation.name, schema: GeolocationSchema }])
-  ],
-  controllers: [GeolocationController],
-  providers: [GeolocationService],
-  exports: [GeolocationService]
-})
-export class GeolocationModule {}
+import { Module } from '@nestjs/common';\nimport { HttpModule } from '@nestjs/axios';\nimport { MongooseModule } from '@nestjs/mongoose';\nimport { GeolocationService } from './geolocation.service';\nimport { GeolocationController } from './geolocation.controller';\nimport { Geolocation, GeolocationSchema } from './schemas/geolocation.schema';\nimport { DistanceCalculatorService } from './distance-calculator.service';\nimport { RouteCalculatorService } from './route-calculator.service';\nimport { TravelZoneService } from './travel-zone.service';\n\n@Module({\n  imports: [\n    HttpModule,\n    MongooseModule.forFeature([{ name: Geolocation.name, schema: GeolocationSchema }])\n  ],\n  controllers: [GeolocationController],\n  providers: [\n    GeolocationService, \n    DistanceCalculatorService,\n    RouteCalculatorService,\n    TravelZoneService\n  ],\n  exports: [\n    GeolocationService, \n    DistanceCalculatorService,\n    RouteCalculatorService,\n    TravelZoneService\n  ]\n})\nexport class GeolocationModule {}
